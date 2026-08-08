bd6c1908cbe373ab84dde8f0f29e82e771edd05e
Zeel-Sutariya-24 <zeelsutariya24@gmail.com>
2026-08-08 10:52:16 +0530

    Update atsController.js
    AI Model updated

--- backend/controllers/atsController.js
+++ backend/controllers/atsController.js
@@ -7,7 +7,7 @@ export const atsCheck = async (req, res) => {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-2.0-flash"
+      model: "gemini-2.5-flash"
     });
 
     const prompt = `
